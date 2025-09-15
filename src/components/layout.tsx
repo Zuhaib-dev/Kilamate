fbf2f0a3e623fcb2ff8a0a2682901df55a8b55b4
Zuhaib Rashid <xuvaxuva006@gmail.com>
2025-09-15 10:46:40 +0530

    Added portfolio URL in footer

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -11,7 +11,7 @@ export function Layout({ children }: PropsWithChildren) {
       <footer className="border-t backdrop-blur supports-[backdrop-filter]:bg-background/60 py-12">
         <div className="container mx-auto px-4 text-center text-gray-200">
           <p>Made with <span className="text-2xl">💗</span> by <span id="owner">
-    <a href="https://zuhaib-portfolio-tau.vercel.app/" target="_blank">Zuhaib </a>
+    <a href="https://www.zuhaibrashid.com/" target="_blank">Zuhaib </a>
   </span>
           </p>
         </div>
