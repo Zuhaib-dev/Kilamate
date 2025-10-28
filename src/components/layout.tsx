39698c87c21ef21a152a2d9fef5925b6526c27a9
Zuhaib Rashid <xuvaxuva006@gmail.com>
2025-10-28 13:09:18 +0530

    fixed The Footer section casuse while toggleing theme from dark to white it was gets transluecent , so fixed the issue

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -9,10 +9,14 @@ export function Layout({ children }: PropsWithChildren) {
         {children}
       </main>
       <footer className="border-t backdrop-blur supports-[backdrop-filter]:bg-background/60 py-12">
-        <div className="container mx-auto px-4 text-center text-gray-200">
-          <p>Made with <span className="text-2xl">💗</span> by <span id="owner">
-    <a href="https://www.zuhaibrashid.com/" target="_blank">Zuhaib </a>
-  </span>
+        <div className="container mx-auto px-4 text-center text-gray-700 dark:text-gray-300">
+          <p>
+            Made with <span className="text-2xl">💗</span> by{" "}
+            <span id="owner">
+              <a href="https://www.zuhaibrashid.com/" target="_blank">
+                Zuhaib{" "}
+              </a>
+            </span>
           </p>
         </div>
       </footer>
