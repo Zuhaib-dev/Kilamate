273cf21ddf6dbf87f5c376dffdbe5988d810fcbb
Zuhaib Rashid <xuvaxuva006@gmail.com>
2026-02-07 14:18:21 +0530

    feat: Implement Header component with navigation, logo, and various utility controls.

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -12,7 +12,7 @@ export function Header() {
   return (
     <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 py-2">
       <div className="container mx-auto flex h-16 items-center justify-between px-4">
-        <Link to={"/"}>
+        <Link to={"/"} className="shrink-0">
           <img
             src={theme === "dark" ? "/logo.webp" : "/logo2.webp"}
             alt="Klimate logo"
