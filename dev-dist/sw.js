404b43560c1d51b298658cb2d5596201bdd2936f
Zuhaib Rashid <xuvaxuva006@gmail.com>
2026-04-15 11:05:37 +0530

    getting into a. new feature

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -83,7 +83,7 @@ define(['./workbox-137dedbd'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.tq5nnetk6ao"
+    "revision": "0.nkolbdb4bf"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
