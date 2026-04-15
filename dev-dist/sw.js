a2df76ee6980a5c093991932beb50bce5e8361b1
Zuhaib Rashid <xuvaxuva006@gmail.com>
2026-04-15 14:47:51 +0530

    some bugs need ti get my head out

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -83,7 +83,7 @@ define(['./workbox-137dedbd'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.loko9svlkv8"
+    "revision": "0.2vci8ob8jcs"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
