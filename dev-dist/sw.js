a7e123d4d77c1c65bb07e168be5bfbf9e6b4740a
Zuhaib Rashid <xuvaxuva006@gmail.com>
2026-04-18 18:14:43 +0530

    feat: add Google Fonts and update service worker revision for index.html

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -83,7 +83,7 @@ define(['./workbox-137dedbd'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.hmkfaekm284"
+    "revision": "0.gk7d2rndmbk"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
