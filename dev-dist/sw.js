c095b46e48d123574533804f3d09a09c5011aa52
Zuhaib Rashid <xuvaxuva006@gmail.com>
2026-04-29 08:41:29 +0530

    chore: update index.html revision hash in service worker manifest

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -83,7 +83,7 @@ define(['./workbox-137dedbd'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.toqqgtr42v8"
+    "revision": "0.k8g3d3sqi1k"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
