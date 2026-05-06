356566a41a290f3a8c04d99e587cbd42622b68f2
Zuhaib Rashid <xuvaxuva006@gmail.com>
2026-05-06 18:02:39 +0530

    new day

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -83,7 +83,7 @@ define(['./workbox-137dedbd'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.n67nselstts"
+    "revision": "0.trb31fqce88"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
