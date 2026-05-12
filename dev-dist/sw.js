02f9712e3183c28b676da3c842a7a5c1b450bea0
Zuhaib Rashid <xuvaxuva006@gmail.com>
2026-05-12 08:55:20 +0530

    new day will work on Making this work faster

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -83,7 +83,7 @@ define(['./workbox-137dedbd'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.742abg060tk"
+    "revision": "0.tcvan6ej0jo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
